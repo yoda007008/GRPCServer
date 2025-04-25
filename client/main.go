e9ac861944d3ad86d80f77da01c919974f72c98a
Kirill Petrovskiy <masteryoda01010101@gmail.com>
2025-04-25 20:36:27 +0300

    create client-server on grpc

--- client/main.go
+++ client/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	pb "testGRPC/gen/proto"
+	"time"
+)
 
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Ошибка подключения")
+	}
+	defer conn.Close()
+
+	client := pb.NewUserServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := client.GetUser(ctx, &pb.UserRequest{Id: "123"})
+	if err != nil {
+		log.Fatalf("Получаем данные от пользователя ID: %s, Name: %s, Email: %s", res.Id, res.Name, res.Email)
+	}
 }
